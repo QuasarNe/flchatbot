07603fc6d190ae6988beb8ff7d37bd3b88f754b8
QuasarNe <rainsipu@gmail.com>
2025-08-07 13:09:48 +0800

    Update flchatbot.py

--- flchatbot.py
+++ flchatbot.py
@@ -1,24 +1,38 @@
 import jieba.posseg as pseg
-#import COPYRIGHT as cp
-rv=1
+import COPYRIGHT as cp
+from time import sleep
+rv=0
 j=False
+
 def initialize():
     global j
     if(j==False):
         print('[语义分析模块加载成功]')
         j=True
 def 评价系统(ch):
-    global rv
-    if(rv==1):
-        print(f"你觉得{ch}怎么样？")
+    global rv,words,run,mark
     op='东西'
     if "ns" in part:op='地方'
     if 'nr' in part:op='人'
-    if 1<rv<=3:
+    if(rv==0):
+        print(f"我不知道{ch}是什么，你能说说吗？")
+        run=True
+    elif(rv==1):
+        print(f"你觉得{ch}怎么样？")
+        run=True
+    elif 1<rv<=3:
         print(f'我知道了，{ch}是个坏{op}')
+        mark=ch
+        run=True
+    elif rv==4:
+        run=False
+        if mark not in words:
+            rv=0
     elif rv==5:
         print(f'知道了知道了，{ch}是个好{op}')
-        rv=1
+        rv=0
+        run=True
+
 def 绘图系统():
     import matplotlib.pyplot as plt
     import numpy as np
@@ -56,27 +70,29 @@ def 绘图系统():
         Q=input('继续吗？(Y/n)')
         if Q=='n':break
 
-#cp.cop("flchatbot")
+cp.cop("flchatbot")
+lw = None  # 添加初始化
 x=input("请键入以开始对话……\n>")
 while(True):
     run=False
     words = pseg.lcut(x)
     initialize()
     for word, part in words:
-        i=0
         if word=='函数' or 'func' in word:
             绘图系统()
             break
         if 'n' in part and part!='eng':
-            if part=='ns' or 'nr':
-                run=True   
-                lw=word
-                if(i>0 and lw==words[i].word): continue
-                print(f'跟我聊聊{word}')
-                i+=1
+            #if part=='ns' or 'nr':
+                if lw is not None and lw in word: 
+                    continue
+                lw=word   
+                #print(f'跟我聊聊{word}')
+                评价系统(word)
+                rv+=1
                 break
-            else:
-                print(f"我不知道{word}是什么，你能说说吗？")
+            #else:
+                评价系统(word)
+                rv+=1
                 run=True
         elif part=='l': 
             print(word)
@@ -85,6 +101,7 @@ while(True):
     if run!=True:
         if (len(words))<=1:print(f'{x}甚')
         else:print("Knowledge is key to the universe,I'll learn more")
-    #print(words)
+    print(words)
     #print('rv=',rv,len(words),word)
     x=input('>')
+    sleep(1)
